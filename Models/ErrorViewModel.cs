c38d41ed51baa337b0dca7c66cdc6e0b0f351abf
George Akpovwovwo <gakpovwovwo@gmail.com>
2024-05-16 12:33:35 +0100

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace LeaveManagement.Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
